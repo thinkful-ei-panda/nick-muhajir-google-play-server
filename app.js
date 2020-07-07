b8852a2216a41e18872d6238f30e7b223d492210
nickthorpe71 <nickthorpe71@gmail.com>
2020-07-07 11:37:18 -0500

    adds both assignments

--- app.js
+++ app.js
@@ -11,7 +11,45 @@ app.get('/', (req, res) => {
 });
 
 app.get('/apps', (req, res) => {
-  res.send(data);
+  const { sort, genres } = req.query;
+  let dataClone = [...data];
+
+  if (sort && sort !== 'Rating' && sort !== 'App') {
+    return res
+      .status(400)
+      .send('Sort must include Rating or App');
+  }
+
+  if (sort) {
+    dataClone.sort((a, b) => {
+      //1, 0, -1
+
+      const newA = a[sort];
+      const newB = b[sort];
+
+      if (newA > newB) {
+        return 1;
+      } else if (newA < newB) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  const allGenres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
+
+  if (genres && !allGenres.includes(genres)) {
+    return res
+      .status(400)
+      .send('Genre must include Action, Puzzle, Strategy, Casual, Arcade, Card');
+  }
+
+  if (genres) {
+    dataClone = dataClone.filter(item => item.Genres === genres);
+  }
+
+  res.send(dataClone);
 });
 
 app.listen(8000, () => {
